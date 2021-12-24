9ffd4945c836a5475d0244340d7b53fb7c784df2
Lento <lento.manickathan@gmail.com>
2021-12-24 08:52:59 +0100

    made fault tolerant. sometime there is readtimeouterror exception when syncing to database

--- log.py
+++ log.py
@@ -85,21 +85,24 @@ def send_alert(readings):
                 
 # Function to write the reading to influxdb
 def write_to_influxdb(readings):
-    with InfluxDBClient(url=f"{config['db_host']}:{config['db_port']}",
-                        token=secrets['token'],
-                        org=config['db_org']) as client:
-        write_api = client.write_api(write_options=SYNCHRONOUS)
-        for r in readings:
-            name = [key for key, value in DEFAULT_DEVICES.items() if value.upper() == r['mac'].upper()]
-            point = Point(name[0] if len(name) > 0 else r['mac']) \
-                .field("CO2 (ppm)", r['co2']) \
-                .field("T (°C)", r['temp']) \
-                .field("RH (%)", r['humi']) \
-                .field("P (Pa)", r['press']) \
-                .field("Ambient Light (ADC)", r['photo']) \
-                .field("Battery (mV)", r['batt']) \
-                .time(datetime.utcnow(), WritePrecision.NS)
-            write_api.write(config['db_bucket'], config['db_org'], point)
+    try:
+        with InfluxDBClient(url=f"{config['db_host']}:{config['db_port']}",
+                            token=secrets['token'],
+                            org=config['db_org']) as client:
+            write_api = client.write_api(write_options=SYNCHRONOUS)
+            for r in readings:
+                name = [key for key, value in DEFAULT_DEVICES.items() if value.upper() == r['mac'].upper()]
+                point = Point(name[0] if len(name) > 0 else r['mac']) \
+                    .field("CO2 (ppm)", r['co2']) \
+                    .field("T (°C)", r['temp']) \
+                    .field("RH (%)", r['humi']) \
+                    .field("P (Pa)", r['press']) \
+                    .field("Ambient Light (ADC)", r['photo']) \
+                    .field("Battery (mV)", r['batt']) \
+                    .time(datetime.utcnow(), WritePrecision.NS)
+                write_api.write(config['db_bucket'], config['db_org'], point)
+    except:
+        pass
         
 # Function to run all functions        
 async def run():
