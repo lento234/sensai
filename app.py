7f2ab236ee39c8452c00f6a214170ad31c60a054
Lento Manickathan <lento.manickathan@gmail.com>
2022-02-27 16:29:23 +0100

    changed name

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ app.layout = html.Div(
     [
         html.H1("SensAI"),
         html.Div(
-            [dcc.Markdown("**INFO:** Recording paused!!", className="last-updated")],
+            [dcc.Markdown("**STATUS:** Recording paused!!", className="last-updated")],
             style={"color": "red"},
         ),
         html.Div([dcc.Markdown(id="last-updated", className="last-updated")]),
